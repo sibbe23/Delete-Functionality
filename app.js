bcbd60297860ce53ca4e2a5026a14c4752c6e724
sibbe23 <sibbe23@gmail.com>
2023-02-28 17:52:45 +0530

    Delete Functionality

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const errorController = require('./controllers/error');
 const db = require('./util/database');
 
 const app = express();
-
+ 
 app.set('view engine', 'ejs');
 app.set('views', 'views'); 
 
